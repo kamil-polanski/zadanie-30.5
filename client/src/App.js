94858973426224fd2f409b5c0b7af4f9a719b38b
Kamil Polanski <polanski88@gmail.com>
2019-10-19 22:50:27 +0200

    Add pages: singlePost; addPost

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/pages/Home/HomePage';
 import Posts from './components/pages/Posts/PostsPage';
 import Contact from './components/pages/Contact/ContactPage';
+import AddPost from './components/pages/AddPost/AddPost';
+import SinglePost from './components/pages/SinglePost/SinglePostPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
+
 
 class App extends React.Component {
 
@@ -15,7 +18,10 @@ class App extends React.Component {
           <Route path="/" exact component={Home} />
           <Route path="/posts" exact component={Posts} />
           <Route path="/contact" exact component={Contact} />
+          <Route path="/posts/new" exact component={AddPost} />
+          <Route path="/posts/:id" exact component={SinglePost} />
           <Route component={NotFound} />
+          
         </Switch>
       </MainLayout>
     );
--- client/src/components/layout/MainLayout/MainLayout.js
+++ client/src/components/layout/MainLayout/MainLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../../features/NavBar/NavBar';
+import NavBar from '../../features/NavBar/Navbar';
 
 const MainLayout = ({ children }) => (
   <div>
