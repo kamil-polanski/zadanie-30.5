b668b2fdd2902a47664da6bb2a5da725cb4d55b6
Kamil Polanski <polanski88@gmail.com>
2019-10-19 20:52:57 +0200

    Add connection to server

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ posts: res });
+      });
+  };
+
   render() {
     return (
       <div>
-        Hello world!
+        <ul>
+          {this.state.posts.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul>
       </div>
     );
   }
